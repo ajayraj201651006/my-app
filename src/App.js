8997f80573ace562b522edcf212f3403983321d3
ajayraj201651006 <ajayraj003016@gmail.com>
2018-12-16 17:22:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -31,14 +31,21 @@ class App extends Component {
       this.setState({persons:persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: "Ajay", age:19},
-        {name: event.target.value, age:18},
-        {name: "Raj", age:18}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex] 
+    };
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+
+    this.setState( {persons:persons} );
   }
 
   togglePersonHandler = () => {
@@ -65,7 +72,8 @@ class App extends Component {
                     click = {() => this.deletePersonHandler(index)}
                     name = {person.name}
                     age = {person.age}
-                    key = {person.id} />
+                    key = {person.id} 
+                    change = {(event) => this.nameChangedHandler(event, person.id)}/>
          })}
          </div> 
         );
