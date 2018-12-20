3545cbaaf6bfdf0416df3bfe0f601a0c30f93f82
ajayraj201651006 <ajayraj003016@gmail.com>
2018-12-20 18:44:01 +0530

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import classes from './App.css';
+import Cockpit from '../components/Cockpit/Cockpit';
 // import Radium, { StyleRoot } from 'radium';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
 
 class App extends Component {
   state = {
@@ -56,39 +57,20 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = '';
 
     if(this.state.showPersons) {
-      persons = (
-        <div>
-         {this.state.persons.map((person,index) => {
-            return <Person
-                    click = {() => this.deletePersonHandler(index)}
-                    name = {person.name}
-                    age = {person.age}
-                    key = {person.id} 
-                    change = {(event) => this.nameChangedHandler(event, person.id)}/>
-         })}
-         </div> 
-        );
-        
-        btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if(this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red);
-    }
-
-    if(this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
+      persons = <Persons 
+                 persons = {this.state.persons}
+                 clicked = {this.deletePersonHandler}
+                 changed = {this.nameChangedHandler} />;
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hi!! I am a react developer</h1>
-        <p className={assignedClasses.join(' ')}>Hii Guys!!! How are You?</p>
-        <button className={btnClass} onClick={this.togglePersonHandler}>Toggle Persons</button>
+        <Cockpit
+         showPersons = {this.state.showPersons}
+          persons = {this.state.persons}
+          clicked = {this.togglePersonHandler}/>
         {persons}
       </div>
     );
