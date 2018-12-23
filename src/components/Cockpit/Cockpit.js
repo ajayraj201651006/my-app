6db8ee0e9493e62ca919d2a1ec28925c3809e45d
ajayraj201651006 <ajayraj003016@gmail.com>
2018-12-23 17:49:57 +0530

    update

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Auxes';
 
 const cockpit = (props) => {
 
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
 
     if(props.showPersons) {
-       btnClass = classes.Red;
+       btnClass = [classes.Button, classes.Red].join(' ');
     } 
     
     if(props.persons.length <= 2) {
@@ -19,12 +20,12 @@ const cockpit = (props) => {
     }
 
 	return (
-		<div className={classes.Cockpit}>
+		      <Aux>
            <h1>{props.appTitle}</h1>
            <p className={assignedClasses.join(' ')}>Hii Guys!!! How are You?</p>
            <button className={btnClass} 
             onClick={props.clicked}>Toggle Persons</button>
-        </div>
+          </Aux>
 	);
 };
 
