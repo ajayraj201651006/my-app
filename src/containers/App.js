34fc15c783cabffdebacd63bdc4e8cd67bb816a8
ajayraj201651006 <ajayraj003016@gmail.com>
2018-12-23 20:41:50 +0530

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,8 @@ class App extends PureComponent {
                      {id: "aj03", name: "Raj", age: 18}
                    ],
                    otherState: 'something',
-                   showPersons: false
+                   showPersons: false,
+                   toggleClicked: 0
             };
   }
 
@@ -92,8 +93,12 @@ class App extends PureComponent {
 
   togglePersonHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
-  }
+    this.setState((prevState, props) => {
+          return { 
+                 showPersons: !doesShow,
+                 toggleClicked: prevState.toggleClicked + 1
+                 }});
+       }
 
   render() {
     console.log("[App.js] inside render");
