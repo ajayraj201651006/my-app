07f4d71bf742df143e771b98e221540ec1b8e591
ajayraj201651006 <ajayraj003016@gmail.com>
2018-12-23 20:57:21 +0530

    update

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
 import Aux from '../../../hoc/Auxes';
+import PropTypes from 'prop-types';
 // import Radium from 'radium';
 
 class Person extends Component {
@@ -30,5 +31,12 @@ class Person extends Component {
     	);		
 	}
 }
+
+Person.propTypes = {
+	click: PropTypes.func,
+	name: PropTypes.string,
+	age: PropTypes.number,
+	change: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
